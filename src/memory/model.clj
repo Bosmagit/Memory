0b803b874cbf05ef2045d729f42f4f08844eb074
Bosmagit <gmbosma@gmail.com>
2013-06-10 14:39:43 +0200

    Cleaner generate-board method, cleanup unused methods

--- src/memory/model.clj
+++ src/memory/model.clj
@@ -1,33 +1,17 @@
 (ns memory.model
   (:require [noir.session :as session]))
 
+(def init-state {:board empty-board :generated-board generate-board :player 1 :score [0 0] :last-lookup nil :false-lookup nil})
+
 (def empty-board [[\- \- \- \-]
                   [\- \- \- \-]
                   [\- \- \- \-]
                   [\- \- \- \-]])
 
-(def filled-board [[\A \A \B \B]
-                  [\C \C \D \D]
-                  [\E \E \F \F]
-                  [\G \G \H \H]])
+(def generate-board
+  (vec (apply map vector(partition 4 4 (shuffle tiles)))))
 
-(def memory-items (list 'A 'A 'B 'B 'C 'C 'D 'D 'E 'E 'F 'F 'G 'G 'H 'H))
-
-(defn generate-board
-  ([list] (generate-board list [] -1))
-  ([list board row]
-    (let [list-size (count list)]
-    (if (= list-size 0)
-      board
-      (let [random (rand-int list-size)]
-        (let [new-list (if (= random 0)
-                       (drop 1 list)
-                       (reduce conj (take random list) (drop (+ random 1) list)))]
-	      (if (= (mod list-size 4) 0)
-	        (generate-board new-list (conj board [(nth list random)]) (+ row 1))
-         	(generate-board new-list (assoc board row (conj (get board row) (nth list random))) row))))))))
-
-(def init-state {:board empty-board :generated-board filled-board :player 1 :score [0 0] :last-lookup nil :false-lookup nil})
+(def tiles (map char(concat (range 65 73) (range 65 73))))
 
 (defn reset-game! []
   (session/put! :game-state init-state))
@@ -81,41 +65,10 @@
 (defn sameCard? [card1 card2]
   (= card1 card2))
 
-(defn winner-in-rows? [board player]
-  (boolean (some (fn [row] (every? (fn [c] (= c player)) row)) board)))
-
-(defn transposed-board [board]
-  (vec (apply map vector board)))
-
-(defn winner-in-cols? [board player]
-  (winner-in-rows? (transposed-board board) player))
-
-(defn winner-in-diagonals? [board player]
-  (let [diag-coords [[[0 0] [1 1] [2 2]]
-                     [[0 2] [1 1] [2 0]]]]
-    (boolean (some (fn [coords] 
-                     (every? (fn [coord] 
-                               (= player (apply get-board-cell board coord))) 
-                             coords))
-                   diag-coords))))
-
 (defn winner?
   ([] false)
   ([board] false)
   ([board player] false))
-
-(defn winnerold?
-  "checks if there is a winner. when called with no args, checks for player X and player O.
-returns the character for the winning player, nil if there is no winner"
-  ([] (winner? (get-board)))
-  ([board]
-    (boolean (or (winner? board \X)
-                 (winner? board \O))))
-  ([board player]
-    (if (or (winner-in-rows? board player)
-            (winner-in-cols? board player)
-            (winner-in-diagonals? board player))
-      player)))
 
 (defn full-board?
   ([] (full-board? (get-board)))
@@ -123,7 +76,7 @@ returns the character for the winning player, nil if there is no winner"
              (not-any? #(= % \-) all-cells))))
 
 (defn new-state [row col old-state]
-  (let [isSameCard (turned-cell-match? (get-last-lookup) [row col]) 
+  (let [isSameCard (turned-cell-match? (get-last-lookup) [row col])
         isDiffirentLookup (not= [row col] (get-last-lookup))
         isNewCard (= (get-board-cell row col) \-)]
   (if (or (and isDiffirentLookup isNewCard) (not= (:false-lookup old-state) nil))
